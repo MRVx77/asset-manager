1a89a098749aa1211c88a9a39c0979b6a2e5bd8e
MRVx77 <vanshdakhode23@gmail.com>
2026-03-04 22:47:21 +0530

    feat: enhance login functionality with session management and Google sign-in integration

--- src/components/auth/login-button.tsx
+++ src/components/auth/login-button.tsx
@@ -1,10 +1,21 @@
 "use client";
 
+import { signIn } from "@/lib/auth-client";
 import { Button } from "../ui/button";
 
 function LoginButton() {
+  const handleLogin = async () => {
+    await signIn.social({
+      provider: "google",
+      callbackURL: "/",
+    });
+  };
+
   return (
-    <Button className="w-full bg-teal-500 hover:bg-teal-600 text-white py-6 text-base font-medium">
+    <Button
+      onClick={handleLogin}
+      className="w-full bg-teal-500 hover:bg-teal-600 hover:cursor-pointer text-white py-6 text-base font-medium"
+    >
       <span>Sing in with google</span>
     </Button>
   );
