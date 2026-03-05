5ae32db8a0ce202b37e0a83c9160f88c4d346cdd
MRVx77 <vanshdakhode23@gmail.com>
2026-03-05 22:06:14 +0530

    feat: implement session management in gallery page to restrict access for admin users

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,17 @@
-function GalleryPage() {
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+async function GalleryPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session && session.user?.role === "admin") {
+    redirect("/");
+  }
+
   return <div>gallary page</div>;
 }
-
+//8.36
 export default GalleryPage;
